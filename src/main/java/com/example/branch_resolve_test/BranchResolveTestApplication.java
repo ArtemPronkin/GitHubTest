d363f9fc4145ab5715b89dfe18a327f41f4e2a93
Artyom <tema.ru2011@gmail.com>
2024-02-12 22:09:45 +0300

    start

---
+++ src/main/java/com/example/branch_resolve_test/BranchResolveTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.branch_resolve_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BranchResolveTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BranchResolveTestApplication.class, args);
+    }
+
+}

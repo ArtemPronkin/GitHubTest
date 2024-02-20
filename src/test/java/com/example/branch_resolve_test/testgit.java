cae1f40621b57c5df3cf2abddd9e1fee8a259f10
Artyom <tema.ru2011@gmail.com>
2024-02-20 20:11:18 +0300

    commit

--- src/test/java/com/example/branch_resolve_test/testgit.java
+++ src/test/java/com/example/branch_resolve_test/testgit.java
@@ -1,7 +1,10 @@
 package com.example.branch_resolve_test;
 
+import java.util.Set;
+
 public class testgit {
     Integer integer;
+    Set set;
 
     public testgit(Integer integer) {
         this.integer = integer;
